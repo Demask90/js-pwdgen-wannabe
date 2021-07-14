d94567d6993e5126f77c1b98244151aede64a872
Demask90 <84012055+Demask90@users.noreply.github.com>
2021-07-14 15:38:46 +0200

    fine bonus

---
+++ bonus/js/main.js
@@ -0,0 +1,8 @@
+
+var nome = prompt("inserisci il tuo nome");
+var cognome = prompt("inserisci il tuo cognome");
+var colore = prompt("inserisci il tuo colore preferito");
+var numero = 21;
+
+document.getElementById("pswgen").innerHTML = nome + cognome + colore + numero;
+
